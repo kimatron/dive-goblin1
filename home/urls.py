44a540e24e464e8150310882023af4d37ce9a343
Kim Hanlon <kimmykim@gmail.com>
2024-07-18 11:37:27 +0000

    Fix url errors and add search and cart  function

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from . import views  # Import your views module
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='home'),  # Example home path
+    path('cart/', views.cart_view, name='cart'),  # Correctly reference views.cart_view
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     # a view to return the home page
     return render(request, 'home/index.html')
+
+def cart_view(request):
+    # Your cart view logic here
+    return render(request, 'cart.html')

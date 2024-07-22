a9a261d43f4bff720404ac0a387ebd6f4f8a576b
Kim Hanlon <kimmykim@gmail.com>
2024-07-22 23:31:15 +0000

    Customize admin :zap:

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from.models import Category, Product
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('sku','name', 'description', 'category', 'price', 'stock','image', )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'friendly_name', 'description', )
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- products/models.py
+++ products/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
+    class Meta:
+        verbose_name_plural = 'categories'
     name = models.CharField(max_length=250)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField(null=True, blank=True)

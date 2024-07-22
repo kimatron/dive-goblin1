c53dc36c994d55f3452799430ad09acc8e7668c6
Kim Hanlon <kimmykim@gmail.com>
2024-07-22 23:13:46 +0000

    Add json files and , update category&product models andple produproducts :sparkle

---
+++ products/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Product(models.Model):
+    sku = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    category = models.ForeignKey('Category', related_name='products', blank=True, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name

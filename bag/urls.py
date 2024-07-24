594f3cda2ed73473e36e0dc208c4dacf386f71d1
Kim Hanlon <kimmykim@gmail.com>
2024-07-24 16:17:09 +0000

    Edit add to bag code and move to bag app for neater code :bug:

--- bag/urls.py
+++ bag/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views  # Import your views module
+from .views import add_to_bag
 
 urlpatterns = [
-    path('', views.view_bag, name='view_bag')
+    path('', views.view_bag, name='view_bag'),
+    path('add-to-bag/<int:product_id>/', add_to_bag, name='add_to_bag'),
     
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+from products.models import Product 
 
 # Create your views here.
 
@@ -8,9 +10,9 @@ def view_bag(request):
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, product_id):
-    """ Add quantity of prduct to bag """
     product = get_object_or_404(Product, id=product_id)
     quantity = int(request.POST.get('quantity', 1))
+    redirect_url = request.POST.get('redirect_url', reverse('view_bag'))  # Default redirect to a shopping bag view
     
     bag = request.session.get('bag', {})
     if product_id in bag:
@@ -19,4 +21,4 @@ def add_to_bag(request, product_id):
         bag[product_id] = quantity
 
     request.session['bag'] = bag
-    return redirect(reverse('product_detail', args=[product_id]))
+    return redirect(redirect_url)

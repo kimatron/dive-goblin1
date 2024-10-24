ddd358f0e880ad8e0d1fb676670a766b54f68020
Kim Hanlon <kimmykim@gmail.com>
2024-10-24 14:18:22 +0000

    Make migrations and create the UserProfile model :sparkle: :construction:

--- profiles/models.py
+++ profiles/models.py
@@ -1,8 +1,15 @@
+# profiles/models.py
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.signals import post_save
+from django.dispatch import receiver
 from django_countries.fields import CountryField
 
 class UserProfile(models.Model):
+    """
+    A user profile model for maintaining default
+    delivery information and order history
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
@@ -14,3 +21,13 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()

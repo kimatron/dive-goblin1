8e22476b5c81549614ba69c1ffd4563b5efde214
Kim Hanlon <kimmykim@gmail.com>
2024-11-06 13:48:08 +0000

    Add stripe checkout views/urls/webhooks etc :sparkle:

--- checkout/views.py
+++ checkout/views.py
@@ -1,24 +1,88 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
-
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from bag.contexts import bag_contents
-
-# import stripe
-
+import stripe
+import json
+@require_POST
+def cache_checkout_data(request):
+    """
+    A view function that handles caching checkout data \
+    for Stripe PaymentIntents.
+    It retrieves data from a POST request, modifies the PaymentIntent metadata,
+    and returns an HTTP response with a status code of 200 if successful.
+    If an exception occurs, it adds an error message \
+    to the Django messages framework and returns an HTTP response \
+    with a status code of 400.
+    """
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    # add your logic here
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
-
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
     }
-
     return render(request, template, context)
+def checkout_success(request, order_number):
+    """
+    A view function called upon successful completion of the checkout process.
+    It retrieves order details, associates the order with the user's profile
+    if authenticated, saves user information \
+    if the "save info" option was selected during checkout, \
+    and displays a success message confirming the order's success.
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+    if 'bag' in request.session:
+        del request.session['bag']
+    context = {
+        'order': order,
+    }
+    return render(request, 'checkout/checkout_success.html', context)

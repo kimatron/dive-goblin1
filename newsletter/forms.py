b0d4ba2bafc28a764369be9fb6873834fe7b6dae
Kim Hanlon <kimmykim@gmail.com>
2024-11-12 00:20:34 +0000

    Add forms for newsletter :sparkle:

---
+++ newsletter/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import NewsletterSubscriber, Newsletter
+
+class NewsletterForm(forms.ModelForm):
+    class Meta:
+        model = NewsletterSubscriber
+        fields = ['email']
+        widgets = {
+            'email': forms.EmailInput(attrs={
+                'class': 'newsletter-input',
+                'placeholder': 'YOUR EMAIL ADDRESS'
+            })
+        }
+
+class NewsletterCreationForm(forms.ModelForm):
+    class Meta:
+        model = Newsletter
+        fields = ['subject', 'content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 10}),
+        }

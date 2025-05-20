10ccf1da6d66aa0e9ec42d8e5fb0659d21d4e476
kimatron <kimmykim@gmail.com>
2025-05-20 20:18:22 +0100

    Enhance webhok with html email support, fix type in payment failed

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -17,20 +17,31 @@ class StripeWH_Handler:
         self.request = request
 
     def _send_confirmation_email(self, order):
-        """Send the user a confirmation email"""
-        cust_email = order.email
-        subject = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_subject.txt',
-            {'order': order})
-        body = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_body.txt',
-            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-        send_mail(
-            subject,
-            body,
-            settings.DEFAULT_FROM_EMAIL,
-            [cust_email]
-        )
+    """Send the user a confirmation email"""
+    cust_email = order.email
+    subject = render_to_string(
+        'checkout/confirmation_emails/confirmation_email_subject.txt',
+        {'order': order})
+    
+    # Plain text email body
+    body = render_to_string(
+        'checkout/confirmation_emails/confirmation_email_body.txt',
+        {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+    
+    # HTML email body
+    html_body = render_to_string(
+        'checkout/confirmation_emails/confirmation_email_body.html',
+        {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+    
+    # Send email with HTML content
+    send_mail(
+        subject,
+        body,
+        settings.DEFAULT_FROM_EMAIL,
+        [cust_email],
+        html_message=html_body,
+        fail_silently=False,
+    )
 
     def handle_event(self, event):
         """
@@ -148,10 +159,10 @@ class StripeWH_Handler:
             status=200
         )
 
-    def handle_payment_intent__payment_failed(self, event):
-        """
-        Handle the payment_intent.payment_failed webhook from Stripe
-        """
-        return HttpResponse(
-            content=f'Webhook received: {event["type"]}',
-            status=200)
+def handle_payment_intent_payment_failed(self, event):
+    """
+    Handle the payment_intent.payment_failed webhook from Stripe
+    """
+    return HttpResponse(
+        content=f'Webhook received: {event["type"]}',
+        status=200)

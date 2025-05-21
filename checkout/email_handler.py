e8bf6c9eb489713b93635542a789c4a86ed2821b
kimatron <kimmykim@gmail.com>
2025-05-21 10:54:43 +0100

    Confirm email configuration, add newsletter email template styling

--- checkout/email_handler.py
+++ checkout/email_handler.py
@@ -4,18 +4,27 @@ from django.conf import settings
 
 def send_confirmation_email(order):
     """Send the user a confirmation email"""
-    subject = f'Dive Goblin - Order Confirmation {order.order_number}'
+    # Get the subject from template or use a fixed one
+    subject = render_to_string(
+        'checkout/confirmation_emails/confirmation_email_subject.txt',
+        {'order': order}).strip()
     
-    # Render email text content
+    # Plain text email body
     body = render_to_string(
-        'checkout/confirmation_emails/confirmation_email_body.html',
-        {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL}
-    )
+        'checkout/confirmation_emails/confirmation_email_body.txt',
+        {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
     
-    # Send email
+    # HTML email body
+    html_body = render_to_string(
+        'checkout/confirmation_emails/confirmation_email_body.html',
+        {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+    
+    # Send email with HTML content
     send_mail(
         subject,
         body,
         settings.DEFAULT_FROM_EMAIL,
-        [order.email]
+        [order.email],
+        html_message=html_body,
+        fail_silently=False,
     )

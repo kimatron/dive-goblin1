b93d9900d5e708f85610fe4d811a298b137772ad
kimatron <kimmykim@gmail.com>
2025-06-06 22:27:22 +0100

    Add favicon

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('search/', views.search_results, name='search_results'),
+    path('favicon.ico', views.favicon, name='favicon'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.cache import cache_control
 
+@cache_control(max_age=60 * 60 * 24)  # Cache for 24 hours
+def favicon(request):
+    # SVG emoji favicon
+    svg_content = '''<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+        <text y=".9em" font-size="90">🤿</text>
+    </svg>'''
+    
+    return HttpResponse(svg_content, content_type='image/svg+xml')
 
 def index(request):
     # a view to return the home page
